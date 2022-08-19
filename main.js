5fe68eebdb3ada1afeb0a3ef442ae2c43526e922
yajat23 <89627077+yajat23@users.noreply.github.com>
2022-08-19 19:31:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+video = "";
+
+function preload()
+{
+ video = createVideo('video.mp4');
+ video.hide();
+}
+
+function setup()
+{
+  canvas = createCanvas(480, 380);
+ canvas.center()
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+console.log("Model Loaded!");
+video.loop();
+video.speed(1);
+video.volume(0);
+}
